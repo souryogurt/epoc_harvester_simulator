e276841d0b62f56363bdf12621003f88e915b0bb
Egor Artemov <egor.artemov@gmail.com>
2018-06-29 00:37:45 +0300

    Require "name" argument
    This argument will be used to replace RecordNumber field in sent data.

--- epoc_harvester_simulator/__main__.py
+++ epoc_harvester_simulator/__main__.py
@@ -19,36 +19,43 @@ from pkg_resources import resource_filename, Requirement
 
 __all__ = ['main']
 
-async def send_data(uri, sample):
+
+async def send_data(name, uri, sample):
     async with websockets.connect(uri) as websocket:
         now = 0.0
         while True:
             max_timestamp = 0.0
             for line in sample.splitlines():
-                dump = json.loads(line) 
+                dump = json.loads(line)
+                dump["RecordNumber"] = name
                 for i, frame in enumerate(dump["frames"]):
                     ts = dump["frames"][i]["TIMESTAMP"]
                     max_timestamp = max(max_timestamp, ts)
                     dump["frames"][i]["TIMESTAMP"] = ts + now
                 await websocket.send(json.dumps(dump))
-            now +=max_timestamp
+            now += max_timestamp
+
 
 def main():
     """Entry point."""
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--verbose', action='store_true', help='be verbose')
+    parser.add_argument('name', help="Name of headset")
     parser.add_argument('url', help='URL to connect to')
     args = parser.parse_args()
     if args.verbose:
         print('I am verbose')
 
-    data_path = resource_filename(Requirement.parse("epoc_harvester_simulator"), "data")
+    data_path = resource_filename(
+        Requirement.parse("epoc_harvester_simulator"), "data")
     sample_file = os.path.join(data_path, 'sample.json')
     sample = None
     with open(sample_file) as sample_file:
         sample = sample_file.read()
 
-    asyncio.get_event_loop().run_until_complete(send_data(args.url, sample))
+    asyncio.get_event_loop().run_until_complete(
+        send_data(args.name, args.url, sample))
+
 
 if __name__ == "__main__":
     sys.exit(main())
